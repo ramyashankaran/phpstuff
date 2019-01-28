4ce47b88ca9e9d226c2fa469bbc8ebc8f1baaa4b
Rajesh Radhakrishnan <Rajesh Radhakrishnan@RR2.fios-router.home>
2019-01-28 00:39:02 -0500

    Added folder and moved animals.php to new folder

---
+++ Animals/Lion.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class Lion extends Animal
+{
+    private $name;
+    
+    public function __construct($family,$food) {
+        parent::Animal($family,$food);
+    }
+   
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+?>
